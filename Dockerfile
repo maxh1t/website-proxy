630c472da5eb275a2a84f19ce3e4d2415bf21fbf
dodevwork <dodevwork@gmail.com>
2024-11-09 22:57:14 +0800

    feat: delete https and try http only

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
 FROM nginx:latest
 
-RUN apt-get update && \
-    apt-get install -y certbot python3-certbot-nginx cron && \
-    rm -rf /var/lib/apt/lists/* \
-
 COPY nginx.conf /etc/nginx/nginx.conf
+
 COPY entrypoint.sh /entrypoint.sh
-
 RUN chmod +x /entrypoint.sh
-RUN echo "0 3 * * * certbot renew --quiet && nginx -s reload" >> /etc/crontab
 
-EXPOSE 80 443
+EXPOSE 80
 
 ENTRYPOINT ["/entrypoint.sh"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-envsubst '${DOMAIN_NAME} ${CLOCK_APP_NAME} ${MAIN_APP_PORT} ${CLOCK_APP_PORT}' < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+# Substitute environment variables in nginx template
+envsubst '$DOMAIN_NAME $MAIN_APP_PORT $CLOCK_APP_NAME $CLOCK_APP_PORT' < /etc/nginx/nginx.conf > /tmp/nginx.conf.temp
 
-if [ ! -d "/etc/letsencrypt/live/${DOMAIN_NAME}" ]; then
-    certbot certonly --nginx --non-interactive --agree-tos --email ${EMAIL} -d ${DOMAIN_NAME} -d www.${DOMAIN_NAME}
+# Verify that the substitution was successful
+if [[ -f /tmp/nginx.conf.temp ]]; then
+    echo "Substitution successful. Moving configuration file."
+    mv /tmp/nginx.conf.temp /etc/nginx/nginx.conf
+else
+    echo "Error: envsubst failed to create the substituted nginx.conf file."
+    exit 1
 fi
-if [ ! -d "/etc/letsencrypt/live/${CLOCK_APP_NAME}.${DOMAIN_NAME}" ]; then
-    certbot certonly --nginx --non-interactive --agree-tos --email ${EMAIL} -d ${CLOCK_APP_NAME}.${DOMAIN_NAME} -d www.${CLOCK_APP_NAME}.${DOMAIN_NAME}
-fi
 
-crond
-
-nginx -g "daemon off;"
-
+nginx -g 'daemon off;'
