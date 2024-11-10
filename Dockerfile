49dfaf0d00109c6fc3db5bc292c6999d8c40faee
dodevwork <dodevwork@gmail.com>
2024-11-10 11:16:55 +0800

    feat: add https settings

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM nginx:latest
 
+RUN apt-get update && \
+    apt-get install -y certbot python3-certbot-nginx cron && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 COPY nginx.conf /etc/nginx/nginx.conf
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-EXPOSE 80
+EXPOSE 80 443
 
 ENTRYPOINT ["/entrypoint.sh"]
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,21 @@ else
     exit 1
 fi
 
+# Check if certificates already exist, if not, obtain them with Certbot
+if [ ! -d "/etc/letsencrypt/live/$DOMAIN_NAME" ]; then
+    echo "Obtaining SSL certificates for $DOMAIN_NAME and subdomains..."
+    certbot certonly --nginx --non-interactive --agree-tos --email $EMAIL \
+        -d $DOMAIN_NAME -d www.$DOMAIN_NAME -d $CLOCK_APP_NAME.$DOMAIN_NAME -d www.$CLOCK_APP_NAME.$DOMAIN_NAME
+else
+    echo "SSL certificates already exist for $DOMAIN_NAME."
+fi
+
+# Set up automatic renewal using cron
+echo "Setting up automatic SSL certificate renewal..."
+echo "0 0,12 * * * root certbot renew --nginx --quiet && nginx -s reload" > /etc/cron.d/certbot-renew
+chmod 0644 /etc/cron.d/certbot-renew
+crontab /etc/cron.d/certbot-renew
+service cron start
+
+echo "Starting Nginx..."
 nginx -g 'daemon off;'
